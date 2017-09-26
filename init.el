0c747327ff2b2d0ce650ca71de6fb5530d71ad67
O'Brien <obriecon@amazon.com>
2017-09-26 10:45:53 -0700

    plorp

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-(require 'package)
 (setq package-archives
       '(("elpy" . "http://jorgenschaefer.github.io/packages/")
 	("marmalade" . "http://marmalade-repo.org/packages/")
@@ -6,22 +5,32 @@
         ("gnu" . "http://elpa.gnu.org/packages/")
         ("melpa-stable" . "https://stable.melpa.org/packages/")
 	("org" . "Http://orgmode.org/elpa/")))
+
 (package-initialize)
+(require 'package)
+(require 'rainbow-delimiters)
+(require 'linum-relative)
+
+;; All mode preferences
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(fringe-mode -1)
 (global-linum-mode t)
-(require 'linum-relative)
 (linum-relative-on)
-(setq linum-format "%d ")
-(fringe-mode 0)
+(ivy-mode)
+(evil-mode)
+(evil-set-initial-state 'markdown-mode 'emacs)
+
 (setq ring-bell-function 'ignore)
 (fset 'yes-or-no-p 'y-or-n-p)
 
-
-(setq per-buffer-theme/default-theme 'atom-one-dark)
+(load-theme 'base16-tomorrow-night t)
+(setq per-buffer-theme/default-theme 'base16-tomorrow-night)
 (setq per-buffer-theme/themes-alist
 	    '(((:theme . adwaita)
-	       (:modes erc-mode erc-list-menu-mode org-mode))
+	       (:modes erc-mode erc-list-menu-mode org-mode markdown-mode))
 	      ((:theme . gruvbox-dark-hard)
 	       (:modes c-mode c++-mode))
 	      ((:theme . base16-monokai)
@@ -29,58 +38,7 @@
 (setq per-buffer-tHeme/timer-idle-delay 0.1)
 (per-buffer-theme/enable)
 
-
-(defvar sm/fixed-font-name "SF Mono")
-(defvar sm/fixed-font-weight 'medium)
-
 (setq explicit-shell-file-name "/usr/local/Cellar/zsh/5.3.1_1/bin/zsh")
-
-(set-face-attribute
- 'default nil
- :family sm/fixed-font-name
- :weight sm/fixed-font-weight)
-
-(defun find-user-init-file ()
-  "Edit the `user-init-file', in another window."
-  (interactive)
-  (find-file user-init-file))
-
-(defun set-frame-size-according-to-resolution ()
-  (interactive)
-  (if window-system
-  (progn
-    ;; use 120 char wide window for largeish displays
-    ;; and smaller 80 column windows for smaller displays
-    ;; pick whatever numbers make sense for you
-    (if (> (x-display-pixel-width) 1280)
-           (add-to-list 'default-frame-alist (cons 'width 120))
-           (add-to-list 'default-frame-alist (cons 'width 80)))
-    ;; for the height, subtract a couple hundred pixels
-    ;; from the screen height (for panels, menubars and
-    ;; whatnot), then divide by the height of a char to
-    ;; get the height we want
-    (add-to-list 'default-frame-alist 
-         (cons 'height (/ (- (x-display-pixel-height) 200)
-                             (frame-char-height)))))))
-
-(defun goto-line-indentation ()
-  (interactive)
-  (call-interactively 'goto-line)
-  (back-to-indentation))
-
-(defun reload-emacs ()
-  (interactive)
-  (save-buffer)
-  (load-file "~/.emacs.d/init.el"))
-
-(setq mac-command-modifier 'meta)
-(setq mac-option-modifier nil)
-
-(set-frame-size-according-to-resolution)
-(setq fullscreen 800)
-(setq fullheight 800)
-(add-to-list 'default-frame-alist '(fullscreen . fullheight))
-(load-theme 'base16-tomorrow-night t)
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 (global-set-key (kbd "C-c c") 'find-user-init-file)
@@ -88,17 +46,32 @@
 (global-set-key (kbd "C-c r") 'reload-emacs)
 (global-set-key (kbd "C-p") 'clipboard-yank)
 
+(defvar sm/fixed-font-name "SF Mono")
+(defvar sm/fixed-font-weight 'medium)
+(set-face-attribute
+ 'default nil
+ :family sm/fixed-font-name
+ :weight sm/fixed-font-weight)
+
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+
 (global-set-key [M-up] 'beginning-of-defun)
 (global-set-key [M-down] 'end-of-defun)
 
+(add-hook 'markdown-mode-hook 'my-markdown-hook)
 (add-hook 'c++-mode-hook 'my-c-hook)
 (add-hook 'c-mode-hook 'my-c-hook)
-(defun my-c-hook()
-  (face-mode-old-school))
-
 (add-hook 'org-mode-hook 'my-erc-hook)
 (add-hook 'erc-mode-hook 'my-erc-hook)
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode);
+
+(defun my-markdown-hook ()
+  (linum-mode 0))
+
+(defun my-c-hook()
+  (face-mode-old-school))
+
 (defun my-erc-hook ()
   (face-mode-variable)
   (linum-mode 0))
@@ -114,6 +87,21 @@
   (setq buffer-face-mode-face '(:family "SF Pro Text" :height 125))
   (buffer-face-mode))
 
+(defun find-user-init-file ()
+  "Edit the `user-init-file', in another window."
+  (interactive)
+  (find-file user-init-file))
+
+(defun goto-line-indentation ()
+  (interactive)
+  (call-interactively 'goto-line)
+  (back-to-indentation))
+
+(defun reload-emacs ()
+  (interactive)
+  (save-buffer)
+  (load-file "~/.emacs.d/init.el"))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -125,21 +113,18 @@
    [unspecified "#1d1f21" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#81a2be" "#c5c8c6"])
  '(custom-safe-themes
    (quote
-    ("aea30125ef2e48831f46695418677b9d676c3babf43959c8e978c0ad672a7329" "100eeb65d336e3d8f419c0f09170f9fd30f688849c5e60a801a1e6addd8216cb" "c968804189e0fc963c641f5c9ad64bca431d41af2fb7e1d01a2a6666376f819c" "3380a2766cf0590d50d6366c5a91e976bdc3c413df963a0ab9952314b4577299" "cea3ec09c821b7eaf235882e6555c3ffa2fd23de92459751e18f26ad035d2142" "5df96b0536c5d334c1e083f62f5cc7a1e28d056bea82a2eb0d049bc08beadc78" "12670281275ea7c1b42d0a548a584e23b9c4e1d2dabb747fd5e2d692bcd0d39b" "c614d2423075491e6b7f38a4b7ea1c68f31764b9b815e35c9741e9490119efc0" "25c06a000382b6239999582dfa2b81cc0649f3897b394a75ad5a670329600b45" "a4c9e536d86666d4494ef7f43c84807162d9bd29b0dfd39bdf2c3d845dcc7b2e" "e1498b2416922aa561076edc5c9b0ad7b34d8ff849f335c13364c8f4276904f0" default)))
+    ("82b67c7e21c3b12be7b569af7c84ec0fb2d62105629a173e2479e1053cff94bd" "44c566df0e1dfddc60621711155b1be4665dd3520b290cb354f8270ca57f8788" "aea30125ef2e48831f46695418677b9d676c3babf43959c8e978c0ad672a7329" "100eeb65d336e3d8f419c0f09170f9fd30f688849c5e60a801a1e6addd8216cb" "c968804189e0fc963c641f5c9ad64bca431d41af2fb7e1d01a2a6666376f819c" "3380a2766cf0590d50d6366c5a91e976bdc3c413df963a0ab9952314b4577299" "cea3ec09c821b7eaf235882e6555c3ffa2fd23de92459751e18f26ad035d2142" "5df96b0536c5d334c1e083f62f5cc7a1e28d056bea82a2eb0d049bc08beadc78" "12670281275ea7c1b42d0a548a584e23b9c4e1d2dabb747fd5e2d692bcd0d39b" "c614d2423075491e6b7f38a4b7ea1c68f31764b9b815e35c9741e9490119efc0" "25c06a000382b6239999582dfa2b81cc0649f3897b394a75ad5a670329600b45" "a4c9e536d86666d4494ef7f43c84807162d9bd29b0dfd39bdf2c3d845dcc7b2e" "e1498b2416922aa561076edc5c9b0ad7b34d8ff849f335c13364c8f4276904f0" default)))
  '(fci-rule-color "#3E4451")
  '(package-selected-packages
    (quote
-    (linum-relative jsx-mode rainbow-delimiters zenburn-theme ccc bury-successful-compilation projectile gruvbox-theme colemak-evil zenburn counsel swiper ivy per-buffer-theme evil load-theme-buffer-local atom-one-dark-theme color-theme-buffer-local autodisass-java-bytecode json-mode org markdown-mode base16-theme)))
+    (idea-darkula-theme github-modern-theme linum-relative jsx-mode rainbow-delimiters zenburn-theme ccc bury-successful-compilation projectile gruvbox-theme colemak-evil zenburn counsel swiper ivy per-buffer-theme evil load-theme-buffer-local atom-one-dark-theme color-theme-buffer-local autodisass-java-bytecode json-mode org markdown-mode base16-theme)))
  '(quote (package-selected-packages (quote (base16-theme)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold :family "variable-pitch"))))
- '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.8))))
- '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.4))))
- '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.2)))))
+ )
 
 (setq initial-frame-alist
       `((background-color . ,(face-background 'default))
@@ -151,12 +136,6 @@
         (left . 620)        ;; This is overridden by my-center-frame later.
         (cursor-color . "red")
         (mouse-color . "green")))
-(require 'package)
-(package-initialize)
-
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(ivy-mode 1)
 (setq ivy-use-virtual-buffers t)
 (setq enable-recursive-minibuffers t)
 (global-set-key "\C-s" 'swiper)
@@ -175,8 +154,3 @@
 (global-set-key (kbd "C-x l") 'counsel-locate)
 (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
 (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(load-theme 'atom-one-dark t)
-(evil-mode)
-(require 'rainbow-delimiters)
-(linum-relative-mode)
