0229459a39e83491130092967d5b14eb1951c1ed
Connor O'Brien <connorpadraigobrien@gmail.com>
2018-03-10 14:20:03 -0800

    start anew

--- elpa/bury-successful-compilation-20150328.1728/bury-successful-compilation-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "bury-successful-compilation" "20150328.1728" "Bury the *compilation* buffer after successful compilation" 'nil :commit "52da2c07419beceab9b4d426d76adb3dcf2548d1" :keywords '("compilation"))
--- elpa/per-buffer-theme-20160318.1501/per-buffer-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "per-buffer-theme" "20160318.1501" "Change theme according to buffer name or major mode." '((cl-lib "0.5")) :url "https://bitbucket.com/inigoserna/per-buffer-theme.el" :keywords '("themes"))
